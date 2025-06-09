bd5c1f24dbfd4627f571e2364c95071bab2e2a56
Lepp3 <lubaka05@gmail.com>
2025-06-09 17:13:50 +0300

    added fetch for user names and location as well as new user functionality on a button

---
+++ modules/api.js
@@ -0,0 +1,34 @@
+import { API_KEY } from '../config.js';
+
+
+
+
+   async function getFiveUsers(){
+    const request = await fetch('https://randomuser.me/api/?results=5&inc=gender,name,nat,picture,location&noinfo');
+    return request.json();
+    };
+
+
+    async function getLatAndLong(streetName,streetNumber,zipcode,city,country){
+        const request = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${streetName}+${streetNumber}%2C+${zipcode}+${city}%2C+${country}&key=${API_KEY}`);
+        const result = await request.json();
+
+        return result;
+
+    };
+
+
+    async function getWeather(latitude,longitude){
+        const request = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code&current=temperature_2m&current=relative_humidity_2m`);
+        const result = await request.json();
+        return result;
+    }
+
+
+    export default{
+        getFiveUsers,
+        getLatAndLong,
+        getWeather
+    }
+    
+    
---
+++ todo.md
@@ -0,0 +1,18 @@
+api for 5 random users name,location,gender,nationality - https://randomuser.me/api/?results=5&inc=gender,name,nat,location
+
+
+api for getting lat and long based on location - https://api.opencagedata.com/geocode/v1/json?q={streetName}+{streetNumber}%2C+{zip code}+{city name}%2C+{countryName}&key={api key}
+
+api for weather based on lat and long + weather codes = https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}4&current=weather_code&current=temperature_2m&current=relative_humidity_2m
+
+
+
+structure
+api calls obj
+{call5,
+    convertLongLang,
+    callWeather,}
+
+update cards
+func(id1,2,3,4,5)
+take all cards by id
