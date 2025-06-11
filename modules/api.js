b7cc6a2e201f14549f5d0739102494546628bfb4
Lepp3 <lubaka05@gmail.com>
2025-06-11 17:53:33 +0300

    removed unused files and html structure

--- modules/api.js
+++ modules/api.js
@@ -1,10 +1,10 @@
-import { API_KEY } from '../config.js';
+import { API_KEY } from '../constants.js';
 import utils from './utils.js';
 
 
 
 
-   async function getFiveUsers(){
+async function getFiveUsers() {
     const request = await fetch('https://randomuser.me/api/?results=5&inc=gender,name,nat,picture,location&noinfo');
     const results = await request.json();
     let userArr = [];
@@ -12,21 +12,21 @@ import utils from './utils.js';
         userArr.push(utils.composeUserObject(user));
     });
     return userArr;
-    };
+};
 
 
-    async function getGeoInformation(user){
-        const request = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${user.location.streetName}+${user.location.streetNumber}%2C+${user.location.zipcode}+${user.location.city}%2C+${user.location.country}&key=${API_KEY}`);
-        const result = await request.json();
-        const [latitude,longitude] = utils.extractLatAndLong(result.results[0].annotations);
-        user.weather.latitude = latitude;
-        user.weather.longitude = longitude;
-        return user;
+async function getGeoInformation(user) {
+    const request = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${user.location.streetName}+${user.location.streetNumber}%2C+${user.location.zipcode}+${user.location.city}%2C+${user.location.country}&key=${API_KEY}`);
+    const result = await request.json();
+    const [latitude, longitude] = utils.extractLatAndLong(result.results[0].annotations);
+    user.weather.latitude = latitude;
+    user.weather.longitude = longitude;
+    return user;
 
-    };
+};
 
-    async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function getWeather(user, maxAttempts = 3, delayMs = 10000) {
@@ -43,7 +43,7 @@ async function getWeather(user, maxAttempts = 3, delayMs = 10000) {
             if (attempt < maxAttempts) {
                 await delay(delayMs);
             }
-            else{
+            else {
                 throw new Error(`Failed to fetch weather data after ${maxAttempts} attempts`);
             }
 
@@ -52,21 +52,21 @@ async function getWeather(user, maxAttempts = 3, delayMs = 10000) {
 };
 
 
-    async function updateWeatherData(latitude, longitude){
-        const request = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code&current=temperature_2m&current=relative_humidity_2m`);
-        const result = await request.json();
-        let weatherCondition = utils.getWeatherDescription(result.current.weather_code);
-        let temperature = result.current.temperature_2m;
-        let humidity = result.current.relative_humidity_2m;
-        return {weatherCondition, temperature, humidity};
-    }
+async function updateWeatherData(latitude, longitude) {
+    const request = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code&current=temperature_2m&current=relative_humidity_2m`);
+    const result = await request.json();
+    let weatherCondition = utils.getWeatherDescription(result.current.weather_code);
+    let temperature = result.current.temperature_2m;
+    let humidity = result.current.relative_humidity_2m;
+    return { weatherCondition, temperature, humidity };
+}
 
 
-    export default{
-        getFiveUsers,
-        getGeoInformation,
-        getWeather 
-        , updateWeatherData
-    }
-    
+export default {
+    getFiveUsers,
+    getGeoInformation,
+    getWeather
+    , updateWeatherData
+}
+
     
