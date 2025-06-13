30bb648bd297fcac4690d2fd48d4bb64f82f07c9
Lepp3 <lubaka05@gmail.com>
2025-06-13 09:50:00 +0300

    changed structure of script to an IIFE app initialization approach

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-import { clearCachedData } from "./modules/utils.js";
 import { initApp } from "./modules/appInitialization.js";
-import { emptyCardHolder, updateWeatherInfo } from "./modules/dom.js";
+import { attachListeners, setUpRefreshWeatherTimer } from "./modules/dom.js";
 
 
 
-const newUserButton = document.getElementById("refresh-users");
+(async function initApp() {
+  toggleContent(true);
+  attachListeners();
+  await setUpRefreshWeatherTimer();
+  const cardHolder = document.querySelector("#card--holder");
+  const userCardsFragment = document.createDocumentFragment();
+  let users = getCachedData("users");
+  if (users) {
+    users.forEach(user => {
+      const card = createUserCard(user);
+      userCardsFragment.appendChild(card);
+    });
+    cardHolder.appendChild(userCardsFragment);
+    toggleContent(false);
+  } else {
+    try {
+      const baseUsers = await getFiveUsers();
+      users = await buildUserInfo(baseUsers);
+      setCachedData("users",users);
+      
+      users.forEach(user => {
+        const card = createUserCard(user);
+        userCardsFragment.appendChild(card);
+      });
+      cardHolder.appendChild(userCardsFragment);
+      toggleContent(false);
+    } catch (error) {
+      console.log("Initialization error: " + error.message);
+      showError();
+    }
 
-newUserButton.addEventListener("click", () => {
-  clearCachedData();
-  emptyCardHolder();
-  initApp();
-});
-
-const updateWeatherButton = document.getElementById("refresh-weather");
-
-updateWeatherButton.addEventListener("click", async () => {
-  await updateWeatherInfo();
-});
-
-let isUpdating = false;
-
-setInterval(async () => {
-  if (isUpdating) {
-    return;
   }
+})();
 
-  isUpdating = true;
-  try {
-    await updateWeatherInfo();
-  } catch (error) {
-    console.error("Weather update failed:" + error.message);
-  } finally {
-    isUpdating = false;
-  }
-}, 600000)
-
-await initApp();
